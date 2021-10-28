04e40f9ecf36cfca30117c20d7ae6823e7085ca4
Dominik Makowiak <dominikmakowiak@gmail.com>
2021-10-28 20:00:05 +0200

    First commit

---
+++ src/main/java/FoodHelper.java
@@ -0,0 +1,15 @@
+package main.java;
+
+/* TODOs
+import API key from apikey.txt file
+figure out how to get certain items from API
+connect to API
+fetch data from API
+output data
+include server
+add logging
+add flags
+ */
+public class FoodHelper {
+
+}
