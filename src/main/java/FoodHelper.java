ceb49e31bae652a42882e631e9ac283d53136e51
Dominik Makowiak <dominikmakowiak@gmail.com>
2021-11-04 16:50:13 +0100

    Added fetching API key from file, so it isn't public to all users

--- src/main/java/FoodHelper.java
+++ src/main/java/FoodHelper.java
@@ -1,7 +1,9 @@
-package main.java;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /* TODOs
-import API key from apikey.txt file
 figure out how to get certain items from API
 connect to API
 fetch data from API
@@ -11,5 +13,34 @@ add logging
 add flags
  */
 public class FoodHelper {
+    private String API_KEY = "";
+    public static void main(String[] args) {
+        FoodHelper foodHelper = new FoodHelper();
+        foodHelper.getAPIKey();
+    }
 
+    private void getAPIKey() {
+        ClassLoader classLoader = FoodHelper.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("apikey.txt");
+        API_KEY = readFromFile(inputStream);
+        System.out.println(API_KEY);
+    }
+
+    private static String readFromFile(InputStream filename) {
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(filename));
+            String line;
+            while((line = bufferedReader.readLine()) != null) {
+                resultStringBuilder.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return resultStringBuilder.toString();
+    }
+
+    public void setAPI_KEY(String API_KEY) {
+        this.API_KEY = API_KEY;
+    }
 }
